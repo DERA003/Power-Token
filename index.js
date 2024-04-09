3db72901189c1385b428693b7409183a884e12e8
Chidera Umejuru <deraumejuru@gmail.com>
2024-04-10 00:38:59 +0100

    fixed token.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-document.getElementById("calculate_btn").addEventListener("click", function(event) {
-  event.preventDefault()
+// document.getElementById("Calculate_btn").addEventListener("click", function(event) {
+//   event.preventDefault()
 
-let amountPaid = parseFloat(document.getElementById("amount").value);
-let priceOfToken = parseFloat(document.getElementById("token").value);
-let varPercentage = parseFloat(document.getElementById("var").value);
+// let amountPaid = (document.getElementById("amount").value);
+// let priceOfToken = (document.getElementById("token").value);
+// let vatPercentage = (document.getElementById("var").value);
 
-let varAmount = amountPaid * (varPercentage/100);
-let totalToken = amountPaid - varAmount;
-let token = Math.floor(totalToken / priceOfToken);
+// if(isNaN(amountPaid) || isNaN(priceOfToken) || isNaN(vatPercentage)) {
+//   document.getElementById("result").innerText =
+//   "please enter valid numeric value"
+//   return;
+// }
 
-document.getElementById("result").innerText = "result" + token
-});
+// if(priceOfToken === 0){
+//   document.getElementById('result').innerText =
+//     "price of token cannot be zero"
+//     return;
+// }
+
+// let vatAmount = amountPaid * (vatPercentage/100);
+// let totalToken = amountPaid - vatAmount;
+// let token = Math.floor(totalToken / priceOfToken);
+
+// document.getElementById("result").innerText = "result" + token
+// });
+
 
--- token.js
+++ token.js
@@ -7,32 +7,34 @@ function promptUser (message) {
     {
         return userResponse
     }
-}
+  }
+  
 
 function calculatePowerToken() {
     let amountPaid = promptUser(
         "Enter the amount paid(including VAT):"
     );
-
+  
     let unitPrice = promptUser(
         "Enter the price of one power unit in Tokens:"
     );
-
+  
     let vatRate = promptUser(
         "Enter the VAT rate as a percentage:"
     );
-
+  
     //   to calculate the token
-    let vatAmount = totalAmountPaid * (vatRate/100);
+    let vatAmount = amountPaid * (vatRate/100);
     let totalAmountPaid = amountPaid - vatAmount;
     let token = Math.floor(totalAmountPaid/ unitPrice);
-
+  
     let display = document.createElement("div");
     display.classList.add("display");
     display.innerHTML = `${token} tokens`;
-
-document.body.appendChild(display);
-
-}
-
-calculatePowerToken ()
+  
+  document.body.appendChild(display);
+  
+  }
+  
+  calculatePowerToken ()
+  
